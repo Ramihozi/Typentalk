940d991deb2103f613bfbec117b1b3be38865b8e
Ramihozi <55562976+Ramihozi@users.noreply.github.com>
2019-09-27 15:37:31 -0700

    Update content.js

--- content.js
+++ content.js
@@ -1,4 +1,17 @@
 document.addEventListener('keydown', (e) => {
   let audioSpace = new Audio(chrome.runtime.getURL('sound.wav'));
   if (e.keyCode === 32) audioSpace.play();
+  let audioSpace = new Audio(chrome.runtime.getURL('sound.wav'));
+  if (e.keyCode === 32) audioSpace.play();
+  let audioSpace = new Audio(chrome.runtime.getURL('sound.wav'));
+  if (e.keyCode === 32) audioSpace.play();
+  let audioSpace = new Audio(chrome.runtime.getURL('sound.wav'));
+  if (e.keyCode === 32) audioSpace.play();
+  let audioSpace = new Audio(chrome.runtime.getURL('sound.wav'));
+  if (e.keyCode === 32) audioSpace.play();
+  let audioSpace = new Audio(chrome.runtime.getURL('sound.wav'));
+  if (e.keyCode === 32) audioSpace.play();
+  let audioSpace = new Audio(chrome.runtime.getURL('sound.wav'));
+  if (e.keyCode === 32) audioSpace.play();  let audioSpace = new Audio(chrome.runtime.getURL('sound.wav'));
+
 });
