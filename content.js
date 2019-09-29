615e9c196df17aaf214605ddf6186dfb8f733b11
Ramihozi <55562976+Ramihozi@users.noreply.github.com>
2019-09-28 19:01:12 -0700

    Update content.js

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 document.addEventListener('keydown', (e) => {
-  let audioSpace = new Audio(chrome.runtime.getURL('Sound/Letter A.mp3'));
+  let audioSpace = new Audio(chrome.runtime.getURL('Typentalk-master/Sound/Letter A.mp3'));
   if (e.keyCode === 32) audioSpace.play();
   let audioSpace = new Audio(chrome.runtime.getURL('sound.wav'));
   if (e.keyCode === 32) audioSpace.play();
