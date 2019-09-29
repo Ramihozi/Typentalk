c8330df6316945a36e31dcb2582951d2c104f6a5
Ramihozi <55562976+Ramihozi@users.noreply.github.com>
2019-09-28 19:45:06 -0700

    Update content.js

--- content.js
+++ content.js
@@ -1,22 +1,22 @@
 document.addEventListener('keydown', (h) => {
   let audioSpace = new Audio(chrome.runtime.getURL('Letter H.mp3'));
   if (e.keyCode === 72) audioSpace.play();
-}
+
 document.addEventListener('keydown', (a) => {
   let audioSpace = new Audio(chrome.runtime.getURL('Letter H.mp3'));
   if (e.keyCode === 72) audioSpace.play();
-}
+
 document.addEventListener('keydown', (b) => {
   let audioSpace = new Audio(chrome.runtime.getURL('Letter H.mp3'));
   if (e.keyCode === 72) audioSpace.play();
-}
+
 document.addEventListener('keydown', (c) => {
   let audioSpace = new Audio(chrome.runtime.getURL('Letter H.mp3'));
   if (e.keyCode === 72) audioSpace.play();
-}
+
 document.addEventListener('keydown', (d) => {
   let audioSpace = new Audio(chrome.runtime.getURL('Letter H.mp3'));
   if (e.keyCode === 72) audioSpace.play();
-}
+
 
 });
