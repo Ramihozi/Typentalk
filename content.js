bf9cfddc366122e9cb41048f5abc85333525f1be
Ramihozi <55562976+Ramihozi@users.noreply.github.com>
2019-09-28 20:41:31 -0700

    Update content.js

--- content.js
+++ content.js
@@ -1,10 +1,10 @@
 if (e.keyCode == 72) {
        var audio = new Audio('Letter H.mp3');
        audio.play();
-       return false;
+       
    }
 if (e.keyCode == 65) {
        var audio = new Audio('Letter A.mp3');
        audio.play();
-       return false;
+       
    }
