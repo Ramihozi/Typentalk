d8c994a564c171df5dc38ce8f1d38a771a288333
Robert <rdtheriault@yahoo.com>
2019-10-02 11:34:57 -0700

    Update content.js
    Created non anonymous function

--- content.js
+++ content.js
@@ -1,4 +1,11 @@
-document.addEventListener('keydown', (a) => {
-  let audioSpace = new Audio(chrome.runtime.getURL('Sound/Letter A.mp3'));
+//document.addEventListener('keydown', (a) => {
+  //let audioSpace = new Audio(chrome.runtime.getURL('Sound/Letter A.mp3'));
+  //if (a.keyCode === 65) audioSpace.play();
+//});
+
+
+let audioSpace = new Audio(chrome.runtime.getURL('Sound/Letter A.mp3'));
+document.addEventListener('keydown',playAudio);
+function playAudio(e) {
   if (a.keyCode === 65) audioSpace.play();
-});
+}
