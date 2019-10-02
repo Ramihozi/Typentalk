f0ba7930bc9aaefd7fda96a89b564c862368de94
Ramihozi <55562976+Ramihozi@users.noreply.github.com>
2019-10-02 10:54:41 -0700

    Update content.js

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
 document.addEventListener('keydown', (a) => {
-  let audioSpace = new Audio(chrome.runtime.getURL('LetterA.wav'));
+  let audioSpace = new Audio(chrome.runtime.getURL('Letter A.mp3'));
   if (a.keyCode === 65) audioSpace.play();
 });
