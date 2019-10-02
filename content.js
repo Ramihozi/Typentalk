be0362bd9ebf27c806f88809d60833c1677f7773
Robert <rdtheriault@yahoo.com>
2019-10-02 11:38:52 -0700

    Update content.js

--- content.js
+++ content.js
@@ -7,5 +7,5 @@
 let audioSpace = new Audio(chrome.runtime.getURL('Sound/Letter A.mp3'));
 document.addEventListener('keydown',playAudio);
 function playAudio(e) {
-  if (a.keyCode === 65) audioSpace.play();
+  if (e.keyCode === 65) audioSpace.play();
 }
