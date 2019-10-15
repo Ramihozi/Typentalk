3df0b540f6509a5b696b27d866417e6e59b75946
Xakleb <55562990+Xakleb@users.noreply.github.com>
2019-10-15 10:29:18 -0700

    Update content.js
    Added Caps

--- content.js
+++ content.js
@@ -38,6 +38,7 @@ let testX =  chrome.runtime.getURL('Sound/LetterX.mp3');
 let testY =  chrome.runtime.getURL('Sound/LetterY.mp3');
 let testZ =  chrome.runtime.getURL('Sound/LetterZ.mp3');
 let testAlt =  chrome.runtime.getURL('Sound/Altkey.mp3');
+let testCaps = chrome.runtimes.getURl('Sound/Capslockkey.mp3');
 
 
 document.addEventListener('keydown',playAudio);
@@ -69,6 +70,8 @@ function playAudio(e) {
   let audioY = new Audio(testY);
   let audioZ = new Audio(testZ);
   let audioAlt = new Audio(testAlt);
+  let audioCaps = new Audio(testCaps);
+  
   
   if (e.keyCode === 65) audioA.play();
   else if (e.keyCode === 66) audioB.play();
@@ -97,7 +100,7 @@ function playAudio(e) {
   else if (e.keyCode === 89) audioY.play();
   else if (e.keyCode === 90) audioZ.play();
   else if (e.keycode === 18) audioAlt.play();
-  
+  else if (e.keycode === 20) audioCaps.play();
 }
 
 
